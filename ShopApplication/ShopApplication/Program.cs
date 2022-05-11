ea447f622c1e0d1a7fbaeab8eeb8387f6bbda756
audriusn <audrius.narkunas@gmail.com>
2022-05-11 21:22:42 +0300

    ShopApplication

---
+++ ShopApplication/ShopApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using ShopApplication.Services;
+
+var applicationService = new ApplicationServices();
+while (true)
+{
+
+    Console.WriteLine("Enter your command:");
+    var command = Console.ReadLine();
+
+    applicationService.Process(command);
+}
---
+++ ShopApplication/ShopApplication/Services/ShopServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShopApplication.Models;
+
+namespace ShopApplication.Services
+{
+    public class ShopServices
+    {
+        private List<ShopItem> _items;
+        public ShopServices ()
+        {
+            _items = new List<ShopItem> ();
+        }
+        public void Add (string name, decimal price, int quantity)
+        {
+            ShopItem shopItem = new ShopItem ();
+            {
+                shopItem.Name = name;
+                shopItem.Price = price;
+                shopItem.Quantity = quantity;
+            }
+            _items.Add (shopItem);
+        }
+        public void Remove(string name)
+        {
+            _items = _items.Where(si => si.Name != name).ToList();
+        }
+        public List<ShopItem> GetAll()
+        {
+            return _items;
+        }
+        public void Update (string name, int quantity)
+        {
+            var item = _items.First(si => si.Name == name);
+            item.Quantity = quantity;
+        }
+    }
+}

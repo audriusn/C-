37f4206ae4107e3d83582aca29ef3094657947e0
audriusn <audrius.narkunas@gmail.com>
2022-05-11 19:55:49 +0300

    LinQ practice

---
+++ ShopItemList/ShopItemList/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ShopItemList.Models;
+Console.WriteLine("Hello, World!");
+List<ShopItem> shopItems = new List<ShopItem>();
+
+shopItems.Add(new ShopItem()
+{
+    Name = "Ice Cream",
+    Price = 2.2M,
+    Quantity = 10
+});
+shopItems.Add(new ShopItem()
+{
+    Name = "Candy",
+    Price = 0.9M,
+    Quantity = 3
+});
+shopItems.Add(new ShopItem()
+{
+    Name = "Cake",
+    Price = 15.0M,
+    Quantity = 3
+});
+shopItems.Add(new ShopItem()
+{
+    Name = "Expired Sandwitch",
+    Price = 3.0M,
+    Quantity = 3,
+    Expired = true
+}); 
+shopItems.Add(new ShopItem()
+{
+    Name = "Expired Sandwitch",
+    Price = 3.0M,
+    Quantity = 3,
+    Expired = true
+}); ;
+// You cannot simply pritn complex objects
+
+//Console.WriteLine(shopItems);
+//foreach ( ShopItem item in shopItems)
+//{
+//    Console.WriteLine($"{item.Name} {item.Quantity}" );
+//}
+////Using linq
+//shopItems.ForEach(item => Console.WriteLine($"{item.Name} {item.Quantity}"));
+
+//filtered
+//Filter not Expired
+//List<ShopItem> notExpiredShopItemsWirhLinq = shopItems.Where(item => item.Expired == false).ToList();
+
+//notExpiredShopItemsWirhLinq.ForEach(item => Console.WriteLine($"{item.Name} {item.Quantity}"));
+
+
+//Select
+//unique 
+//List<string> uniqueNames = new List<string>();
+//foreach (ShopItem item in shopItems)
+//{
+//    var uniqueName = item.Name;
+//    if(uniqueNames.Contains(uniqueName))
+//    {
+//        uniqueNames.Add(uniqueName);
+//    }
+//}
+
+//List<string> uniqueNamesWithLinq = shopItems.Select(item => item.Name).Distinct().ToList();
+//uniqueNames.ForEach(name => Console.WriteLine(name));
+
+//Function programming
+
+//Get The first item by name that is nor expired
+
+//ShopItem firstShopItem = shopItems.Where(s => !s.Expired).OrderBy(s => s.Name).First();
+//Console.WriteLine(firstShopItem.Name);
+
+// Get the  biggest quantity of an item
+
+int largestQuantity = shopItems.OrderByDescending(s =>s.Quantity).Select(s => s.Quantity).First();
+Console.WriteLine(largestQuantity);
--- ShopManegment/ShopManegmentApp/Services/ApplicationServices.cs
+++ ShopManegment/ShopManegmentApp/Services/ApplicationServices.cs
@@ -14,51 +14,60 @@ namespace ShopManegmentApp.Services
         {
             _shopService = new ShopServices();
         }
+
         public void Process(string command)
         {
-            if (command.StartsWith("Add"))
+            try
             {
-                string[] splitCommand = command.Split(" ");
-       
-                for (int i = 0; i < _shopService.GetAll().Count; i++)
-                { 
-                    if (splitCommand[1].Equals(_shopService.GetAll()[i].Name))
-                    {
-                        Console.WriteLine("Sorry, this item is already in a lits");
-                    }
-                    
-                    _shopService.Add(splitCommand[1], splitCommand[2]);
-                }
-   
-            }
-            else if (command.StartsWith("Remove"))
-            {
-                string[] splitCommand = command.Split(" ");
-                _shopService.Remove(splitCommand[1]);
-            }
-            else if (command.StartsWith("Show"))
-            {
-                List<ShopItem> items = _shopService.GetAll();
 
-                foreach (ShopItem item in items)
+                if (command.StartsWith("Add"))
                 {
-                   
-                    Console.WriteLine($"ItemName: {item.Name} ItemQty: {item.Quantity}");
+                    string[] splitCommand = command.Split(" ");
+
+                    for (int i = 0; i < _shopService.GetAll().Count; i++)
+                    {
+                        if (splitCommand[1].Equals(_shopService.GetAll()[i].Name))
+                        {
+                            Console.WriteLine("Sorry, this item is already in a lits");
+                        }
+
+                        _shopService.Add(splitCommand[1], splitCommand[2]);
+                    }
+
                 }
-            }
-            else if (command.StartsWith("Set"))
-            {
-                string[] splitCommand = command.Split(" ");
-                _shopService.Update(splitCommand[1], splitCommand[2]);
+                else if (command.StartsWith("Remove"))
+                {
+                    string[] splitCommand = command.Split(" ");
+                    _shopService.Remove(splitCommand[1]);
+                }
+                else if (command.StartsWith("Show"))
+                {
+                    List<ShopItem> items = _shopService.GetAll();
+
+                    foreach (ShopItem item in items)
+                    {
+
+                        Console.WriteLine($"ItemName: {item.Name} ItemQty: {item.Quantity}");
+                    }
+                }
+                else if (command.StartsWith("Set"))
+                {
+                    string[] splitCommand = command.Split(" ");
+                    _shopService.Update(splitCommand[1], splitCommand[2]);
+                }
+
+                else if (command.StartsWith("Exit"))
+                {
+                    Environment.Exit(0);
+                }
+                else
+                    Console.WriteLine("Incorret command");
             }
 
-            else if (command.StartsWith("Exit"))
+            catch 
             {
-              Environment.Exit(0);
+                Console.WriteLine("Bad command name. Try again");
             }
-            else
-                Console.WriteLine("Incorret command");
-            
         }
-    }
+    } 
 }
